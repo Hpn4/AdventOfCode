75c019c7d0d44f76ac5b1d5bd5a42918aca66bc0
Hpn4 <senigout.etienne@gmail.com>
2024-12-06 07:45:06 +0100

    feat: aoc 2024 day6

---
+++ 2024/day6.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from helper import findGrid, DIRS
+
+lines = open(0, "r").readlines()
+lines = [[c for c in line.strip()] for line in lines]
+
+# Start position
+s_x, s_y = findGrid(lines, '^')
+
+def run(x, y, lines):
+	d = 3
+	pos = defaultdict(int)
+	pos[(x, y)] = 1
+	while True:
+		xx = x + DIRS[d][0]
+		yy = y + DIRS[d][1]
+
+		if xx < 0 or yy < 0 or xx >= len(lines[0]) or yy >= len(lines):
+			return len(pos), pos.keys()
+
+		if lines[yy][xx] == "#":
+			d = (d + 1) % 4
+		else:
+			x = xx
+			y = yy
+
+			key = (xx, yy)
+			pos[key] += 1
+
+			if pos[key] >= 5:
+				return -1, None
+
+acc, pos = run(s_x, s_y, lines)
+
+acc2 = 0
+for (x, y) in pos:
+	if lines[y][x] != '.':
+		continue
+
+	lines[y][x] = '#'
+
+	if run(s_x, s_y, lines)[0] == -1:
+		acc2 += 1
+
+	lines[y][x] = '.'
+
+print("Part1:", acc)
+print("Part2:", acc2)
